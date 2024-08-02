482923983761f6c86a06b7b8be3a3267823258a0
Serhat Yaman <serhat-yaman@outlook.com>
2024-08-02 04:41:47 +0300

    Prefab ve script güncellendi

--- Canavar Labirenti/Assets/Scripts/Card Scripts/Card_Special_Scipts/Card_Door_Special_Script.cs
+++ Canavar Labirenti/Assets/Scripts/Card Scripts/Card_Special_Scipts/Card_Door_Special_Script.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class Card_Door_Special_Script : MonoBehaviour
     {
         public Selection_Point_interact selection_Point_Interact;
+        public SelectionAction selectionAction;
         public Card_State_script card_State_script;
         public GameObject CardSpecialObject;
 
         // Start is called before the first frame update
         void Start()
             {
-                
+                selectionAction = GameObject.Find("Kroki").GetComponent<SelectionAction>();
             }
 
         // Update is called once per frame
 
 
         public void CardSpecialEffect()
-            {
-                Instantiate(CardSpecialObject, selection_Point_Interact.selectedObjectTransform);
+            {   
+                Debug.Log("Kapı Special effect çalıştı.");
+                selectionAction.PrefabToInstantiate = CardSpecialObject;
             }
 
         public void CardOutEffect()
             {
+                Debug.Log("Kapı Out effect çalıştı.");
                 CardSpecialObject.transform.Translate(CardSpecialObject.transform.up*Time.deltaTime);
                 Destroy ( CardSpecialObject, 2f);
             }
