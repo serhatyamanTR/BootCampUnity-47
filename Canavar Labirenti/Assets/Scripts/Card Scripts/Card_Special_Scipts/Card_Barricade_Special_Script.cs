c5fa11351cad52887fda8fbe4bb494a3bfd15ad3
Samet Koşmaz <ksmzsmt@gmail.com>
2024-08-02 03:14:35 +0300

    Barricade card done

--- Canavar Labirenti/Assets/Scripts/Card Scripts/Card_Special_Scipts/Card_Barricade_Special_Script.cs
+++ Canavar Labirenti/Assets/Scripts/Card Scripts/Card_Special_Scipts/Card_Barricade_Special_Script.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class Card_Barricade_Special_Script : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Selection_Point_interact selection_Point_Interact;
+    public GameObject CardSpecialObject;
+    // Start is called before the first frame update
+    public SelectionAction SelectionAction;
     void Start()
     {
-        
+        selection_Point_Interact = GameObject.Find("Selection_Point").GetComponent<Selection_Point_interact>();
+        SelectionAction=GameObject.Find("Kroki").GetComponent<SelectionAction>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void CardSpecialEffect()
+    {
+        Debug.Log("....card özel efekt çalýþtý");
+        //selection_Point_Interact.selectedObjectTransform.gameObject.SetActive(false);
+        //Instantiate(CardSpecialObject, selection_Point_Interact.selectedObjectTransform.position + Vector3.up, selection_Point_Interact.selectedObjectTransform.rotation);
+        SelectionAction.PrefabToInstantiate=CardSpecialObject;
+        Debug.Log("oluþturma kodu çalýþtý");
+
+
+
+    }
+    public void CardOutEffect()
+    {
+        CardSpecialObject.GetComponent<Renderer>().material.color = Color.red;
+        CardSpecialObject.transform.Translate(CardSpecialObject.transform.up * Time.deltaTime);
+        Destroy(CardSpecialObject, 2f);
     }
 }
